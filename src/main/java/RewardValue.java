77f2cf23181dd0e818be87e42378f7d87fc50633
devpy <deventiwari2029@gmail.com>
2023-09-16 23:03:39 +0530

    forgjprew modified testcase v1.2

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,23 +1,35 @@
 public class RewardValue {
     public static final double CONVERSION_MILES_TO_RATE = 0.0035;
     double cash;
-    int miles;
+    //int miles;
 
     public RewardValue(double cash) {
         this.cash = cash;
     }
     public RewardValue(int miles)
     {
-        this.miles = miles;
+        this.cash = RewardValue.convert_cash(miles);
+    }
+    public static int convert_miles(double cash)
+    {
+        int miles;
+        miles = (int)(cash / RewardValue.CONVERSION_MILES_TO_RATE) ;
+        return miles;
+    }
+
+    public static double convert_cash(int miles)
+    {
+        double cash;
+        cash = RewardValue.CONVERSION_MILES_TO_RATE * miles;
+        return cash;
     }
     public double getCashValue()
     {
-        this.cash = this.miles / RewardValue.CONVERSION_MILES_TO_RATE ;
+        //this.cash = this.miles / RewardValue.CONVERSION_MILES_TO_RATE ;
         return this.cash;
     }
     public int getMilesValue()
     {
-        this.miles = (int) (RewardValue.CONVERSION_MILES_TO_RATE * this.cash);
-        return this.miles;
+        return RewardValue.convert_miles(this.cash);
     }
 }
--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,21 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        int miles = 28571;
+        double cash = 99.9985;
+        var rewardValue = new RewardValue(cash);
+        //assert false;
+        assertEquals(miles, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int miles = 28571;
+        double cash = 99.9985;
+        var rewardValue = new RewardValue(miles);
+        //assert false;
+        assertEquals(cash, rewardValue.getCashValue());
     }
+
+
 }
